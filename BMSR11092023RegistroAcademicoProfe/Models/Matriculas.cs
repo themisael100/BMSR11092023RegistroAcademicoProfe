feaedcddde88f22d537839b8fd0ca0946bdc3a1a
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-11 20:12:46 -0600

    Creacion de controlador: MatriculaController.cs y Matriculas.cs

---
+++ BMSR11092023RegistroAcademicoProfe/Models/Matriculas.cs
@@ -0,0 +1,9 @@
+﻿namespace BMSR11092023RegistroAcademicoProfe.Models
+{
+    public class Matriculas
+    {
+        public int Id { get; set; }
+        public string NameStudent { get; set; }
+        public string Classroom { get; set; }
+    }
+}
